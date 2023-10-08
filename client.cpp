1a34a7b7ad96027164c2a4c3b50d2c59ce55fa54
sutha64 <51185426+sutha64@users.noreply.github.com>
2023-10-08 10:14:25 +0545

    refactored client server code

--- client.cpp
+++ client.cpp
@@ -4,21 +4,21 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 
-#define PORT 8080
+#define servPORT 8080
 
 using namespace std;
 
 int main()
 {
 
-    const char *customIP = "127.0.0.1";
+    const char *servIP = "127.0.0.1";
 
     // Create a socket
     int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
 
     if (clientSocket == -1)
     {
-        std::cerr << "Error creating socket\n";
+        cerr << "Error creating socket\n";
         return -1;
     }
     else
@@ -29,23 +29,21 @@ int main()
     // Set up the address family
     sockaddr_in serverAddress;
     serverAddress.sin_family = AF_INET;
-    serverAddress.sin_port = htons(PORT);
-    serverAddress.sin_addr.s_addr = inet_addr(customIP);
+    serverAddress.sin_port = htons(servPORT);
+    serverAddress.sin_addr.s_addr = inet_addr(servIP);
 
     // Connect to the server
     if (connect(clientSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) == -1)
     {
-        std::cerr << "Error connecting to the server\n";
+        cerr << "Error connecting to the server\n";
         close(clientSocket);
         return -1;
     }
     else
     {
-        std::cout << "Connected to the server at " << customIP << ":" << ntohs(serverAddress.sin_port) << "\n";
+        cout << "Connected to the server at " << servIP << ":" << ntohs(serverAddress.sin_port) << "\n";
     }
-    // ... rest of the code (send/receive data)
 
-    // Close the socket when done
     close(clientSocket);
 
     return 0;

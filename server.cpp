a0c93d71307deb247e563505eee177ebae88e223
sutha64 <surajth64@gmail.com>
2023-11-07 16:19:21 +0545

    changes variable names and output for user

--- server.cpp
+++ server.cpp
@@ -11,45 +11,45 @@ using namespace std;
 int main()
 {
 
-    const char *customIP = "127.0.0.1";
+    const char *servIP = "127.0.0.1";
 
     // Create a socket
-    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    int listening = socket(AF_INET, SOCK_STREAM, 0);
 
-    if (serverSocket == -1)
+    if (listening == -1)
     {
-        cerr << "Error creating socket\n";
+        cerr << "[SYSTEM] Error creating socket\n";
         return -1;
     }
     else
     {
-        cout << "Socket Created Successfully\n";
+        cout << "[SYSTEM] Socket Created Successfully\n";
     }
 
     // Bind the socket to a specific address and port
     sockaddr_in serverAddress;
     serverAddress.sin_family = AF_INET;
     serverAddress.sin_port = htons(PORT);
-    serverAddress.sin_addr.s_addr = inet_addr(customIP);
+    serverAddress.sin_addr.s_addr = inet_addr(servIP);
 
-    if (bind(serverSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) == -1)
+    if (bind(listening, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) == -1)
     {
-        cerr << "Error binding socket\n";
-        close(serverSocket);
+        cerr << "[SYSTEM] Error binding socket\n";
+        close(listening);
         return -1;
     }
     else
     {
-        cout << "Successfully bind to local port\n";
+        cout << "[SYSTEM] Successfully binded to local port\n";
     }
 
-    if (listen(serverSocket, 1) < 0)
+    if (listen(listening, SOMAXCONN) < 0)
     {
-        cout << "Failed to start\n";
+        cout << "[SYSTEM] Failed to start\n";
     }
     else
     {
-        cout << "Started listening to local port\n";
+        cout << "[SYSTEM] Started listening to local port\n";
     }
 
     while (true)
@@ -60,19 +60,19 @@ int main()
         socklen_t clientAddrSize = sizeof(clientAddress);
 
         // Accept a connection from the client
-        int clientSocket = accept(serverSocket, (struct sockaddr *)&clientAddress, &clientAddrSize);
+        int clientSocket = accept(listening, (struct sockaddr *)&clientAddress, &clientAddrSize);
 
         if (clientSocket == -1)
         {
-            cerr << "Error accepting connection";
+            cerr << "[SYSTEM] Error accepting connection";
         }
 
-        cout << "Accepted connection from "
+        cout << "[SYSTEM] Accepted connection from "
              << inet_ntoa(clientAddress.sin_addr) // Convert client IP to string
              << ":" << ntohs(clientAddress.sin_port) << "\n";
 
         close(clientSocket);
     }
-    close(serverSocket);
+    close(listening);
     return 0;
 }

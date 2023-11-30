a7f76b99f17e6e95426b963d37d73c9cea703d2f
sutha64 <surajth64@gmail.com>
2023-11-30 10:39:42 +0545

    refactored some changes in server program

--- src/server.cpp
+++ src/server.cpp
@@ -54,62 +54,61 @@ int main()
             cout << "[SYSTEM] Started listening to local port\n";
       }
 
-            // accepting connection from the client
-            sockaddr_in clientAddress;
-            socklen_t clientAddrSize = sizeof(clientAddress);
-            char hostName[NI_MAXHOST];    // Client's remote name
-            char service[NI_MAXSERV]; // Service (i.e. port) the client is connect on
-            memset(hostName, 0, NI_MAXHOST); // same as memset(host, 0, NI_MAXHOST);
-            memset(service, 0, NI_MAXSERV);
+      // accepting connection from the client
+      sockaddr_in clientAddress;
+      socklen_t clientAddrSize = sizeof(clientAddress);
+      char hostName[NI_MAXHOST];       // Client's remote name
+      char service[NI_MAXSERV];        // Service (i.e. port) the client is connect on
+      memset(hostName, 0, NI_MAXHOST); // same as memset(host, 0, NI_MAXHOST);
+      memset(service, 0, NI_MAXSERV);
 
-            // Accept a connection from the client
-            int clientSocket = accept(listening, (struct sockaddr *)&clientAddress, &clientAddrSize);
+      // Accept a connection from the client
+      int clientSocket = accept(listening, (struct sockaddr *)&clientAddress, &clientAddrSize);
 
-            if (clientSocket == -1)
-            {
-                  cerr << "[SYSTEM] Error accepting connection";
-            }
+      if (clientSocket == -1)
+      {
+            cerr << "[SYSTEM] Error accepting connection";
+      }
 
-            if (getnameinfo((sockaddr *)&clientAddress, clientAddrSize, hostName, NI_MAXHOST, service, NI_MAXSERV, 0) == 0)
-            {
-                  cout <<"[SYSTEM] "<< hostName << " connected on port " << service << endl;
-            }
-            else{
+      if (getnameinfo((sockaddr *)&clientAddress, clientAddrSize, hostName, NI_MAXHOST, service, NI_MAXSERV, 0) == 0)
+      {
+            cout << "[SYSTEM] " << hostName << " connected on port " << service << endl;
+      }
+      else
+      {
             cout << "[SYSTEM] Accepted connection from "
                  << inet_ntoa(clientAddress.sin_addr) // Convert client IP to string
                  << ":" << ntohs(clientAddress.sin_port) << "\n";
-            }
-            while (true)
-            {
-                  size_t messageLen;
-                  int msg = recv(clientSocket, &messageLen, sizeof(messageLen), 0);
+      }
+      while (true)
+      {
+            size_t messageLen;
+            int msg = recv(clientSocket, &messageLen, sizeof(messageLen), 0);
 
-                  if (msg < 0)
-                  {
-                        cerr << "[SYSTEM] Error receiving message length\n";
-                        break;
-                  }
-                  else if (msg == 0)
-                  {
-                        cerr << "[SYSTEM] Client disconnected\n";
-                        break;
-                  }
-else{
+            if (msg < 0)
+            {
+                  cerr << "[SYSTEM] Error receiving message length\n";
+                  break;
+            }
+            else if (msg == 0)
+            {
+                  cerr << "[SYSTEM] Client disconnected\n";
+                  break;
+            }
+            else
+            {
                   char recBuff[messageLen];
                   memset(recBuff, 0, messageLen);
                   msg = recv(clientSocket, recBuff, messageLen, 0);
 
-                  
-                  
-                        recBuff[msg] = '\0'; // Null-terminate the received data
-                        cout << "Message received: " << recBuff << '\n';
-                        memset(recBuff, 0, messageLen);
-                  }
-                  
+                  recBuff[msg] = '\0'; // Null-terminate the received data
+                  cout << "Message received: " << recBuff << '\n';
+                  memset(recBuff, 0, messageLen);
             }
+      }
 
-            close(clientSocket);
-      
+      close(clientSocket);
+
       close(listening);
       return 0;
 }
